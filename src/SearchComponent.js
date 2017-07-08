ff14c240a544cce7cf117afdf99729265717f30f
fade2g <holger.heymanns@gmail.com>
2017-07-08 15:44:49 +0200

    feat: Implemented backend call to retrieve the books
    Note: The books are not displayed but only stored in the state of the
    component
    
    Resolves: #20

--- src/SearchComponent.js
+++ src/SearchComponent.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import * as BooksAPI from './BooksAPI'
 
 class SearchComponent extends Component {
+
+  state = {
+    searchTerm: '',
+    foundBooks: []
+  };
+
+  handleChange = (event) => {
+    this.setState({searchTerm: event.target.value});
+    BooksAPI.search(this.state.searchTerm, 20).then((response) => {
+      this.setState({books: response});
+    })
+  };
+
   render() {
     return <div className="search-books">
       <div className="search-books-bar">
         <Link to="/" className="close-search">Close</Link>
         <div className="search-books-input-wrapper">
-          <input type="text" placeholder="Search by title or author"/>
+          <input type="text" placeholder="Search by title or author" value={this.props.searchTerm} onChange={(event) => (this.handleChange(event))}/>
         </div>
       </div>
       <div className="search-books-results">
